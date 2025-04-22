2244d47b0d7942352f437b8ea88634a9745a3ce0
AmbrizAlberto <albertpoambez@gmail.com>
2025-04-22 14:33:34 -0600

    feat: map view first design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import styles from "../../public/css/map.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hola</h1>
-    </div>
+    <section>
+      <div className="main">
+        <div className="twodivs">
+
+          <div className="leftside">
+            <h1>MAP</h1>
+          </div>
+
+          <div className="rightside">
+            <h2>CONSUME</h2>
+          </div>
+
+        </div>
+      </div>
+    </section>
   );
 }
