8ae6a490bd177179f09de2727f17a7ef63c60f77
AmbrizAlberto <albertpoambez@gmail.com>
2025-05-30 09:44:37 -0600

    fix: for deploy

--- src/app/api/buildings/[id]/route.ts
+++ src/app/api/buildings/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/db';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function GET(request: NextRequest, context: any) {
   const { id } = context.params;
 
